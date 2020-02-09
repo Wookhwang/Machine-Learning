8eaf8517057d4beb3272081cb2f2092687123f3d
Wookhwang <46039975+Wookhwang@users.noreply.github.com>
2020-02-09 23:32:02 +0900

    Add files via upload

---
+++ Support_Vector_Machine/src/SVM_iris.py
@@ -0,0 +1,77 @@
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # 마커와 컬러맵 설정
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 결정 경계를 그립니다.
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black')
+
+    # 테스트 샘플을 부각하여 그립니다.
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100,
+                    label='test set')
+
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+"""
+SVM을 사용하면 과대 적합 문제가 생기는 것을 어느정도 막을 수 있다.
+
+C 값을 줄이면 편향이 늘고 모델 분산이 줄어든다.
+반대로 C 값을 늘리면 편향이 줄고 모델 분산이 늘어난다.
+즉, 마진을 조정하려면 C 값을 조정하면 된다.
+실제로 테스트를 해보면 1과 2의 w의 벡터 값이 변화되는 것을 알 수 있다.
+"""
+svm = SVC(kernel='linear', C=1.0, random_state=1)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
